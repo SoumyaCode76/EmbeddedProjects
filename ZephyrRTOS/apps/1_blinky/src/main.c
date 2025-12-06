7f5660ca1b7b8ed853f47ff589ed34915a508b2e
SoumyaCode76 <soumyadeep.work02@gmail.com>
2025-12-06 19:04:38 +0530

    1. Update the prj.conf so that the final elf contains debug symbols. 2. Set up the launch.json and tasks.json for debugging using    arm-none-eabi-gdb.exe and arm-zephyr-eabi-gdb.exe. Tested with MEMORY    view, Disassembly view, and all VSCode debug features.

--- ZephyrRTOS/apps/1_blinky/src/main.c
+++ ZephyrRTOS/apps/1_blinky/src/main.c
@@ -17,7 +17,7 @@ static const struct gpio_dt_spec led_2 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 
 int main(void)
 {
-    int ret;
+    volatile int ret;
 
     if (!gpio_is_ready_dt(&led_1)) {
         printk("Error: LED device not ready\n");
@@ -43,20 +43,20 @@ int main(void)
     printk("Starting blinky on %s\n", CONFIG_BOARD);
 
     while (1) {
-        ret = gpio_pin_toggle_dt(&led_1);
-        if (ret < 0) {
+        if (gpio_pin_toggle_dt(&led_1) < 0) {
             printk("Error: Could not toggle LED\n");
             return 0;
         }
         printk("Toggled Green LED\n");
-        k_msleep(500);
-        ret = gpio_pin_toggle_dt(&led_2);
-        if (ret < 0) {
+        printk("Green LED status: %d\n", gpio_pin_get_dt(&led_1));        
+        k_msleep(1000);
+        if (gpio_pin_toggle_dt(&led_2) < 0) {
             printk("Error: Could not toggle LED\n");
             return 0;
         }
         printk("Toggled Red LED\n");
-        k_msleep(500);        
+        printk("Red LED status: %d\n", gpio_pin_get_dt(&led_2));
+        k_msleep(1000);        
     }
     return 0;
 }
