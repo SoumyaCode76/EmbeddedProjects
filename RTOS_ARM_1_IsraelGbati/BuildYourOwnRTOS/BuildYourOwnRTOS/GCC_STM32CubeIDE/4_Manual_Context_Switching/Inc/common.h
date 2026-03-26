0be3f3ec2ec378692da7258d9cfb4de6d628d867
SoumyaCode76 <soumyadeep.work02@gmail.com>
2026-03-26 08:29:46 +0530

    Base project with Board configuration done for UART and LED

---
+++ RTOS_ARM_1_IsraelGbati/BuildYourOwnRTOS/BuildYourOwnRTOS/GCC_STM32CubeIDE/4_Manual_Context_Switching/Inc/common.h
@@ -0,0 +1,14 @@
+/*
+ * common.h
+ *
+ *  Created on: 26-Mar-2026
+ *      Author: soumy
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#define SYS_FREQ		16000000
+#define APB2_CLK		SYS_FREQ
+
+#endif /* COMMON_H_ */
